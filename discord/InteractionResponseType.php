1b30b383ec79959c29fdac97d09c3659d7bdec15
Ian Webster <ianw_github@ianww.com>
2021-03-05 11:43:12 -0800

    Update InteractionResponseType

--- discord/InteractionResponseType.php
+++ discord/InteractionResponseType.php
@@ -4,9 +4,7 @@ namespace Discord;
 
 abstract class InteractionResponseType {
   const PONG = 1;
-  const ACKNOWLEDGE = 2;
-  const CHANNEL_MESSAGE = 3;
   const CHANNEL_MESSAGE_WITH_SOURCE = 4;
-  const ACKNOWLEDGE_WITH_SOURCE = 5;
+  const DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5;
 }
 
